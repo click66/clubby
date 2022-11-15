f2856a7024bd01bb2dd1964e56472e604f0f43ea
Clark Sirl <click66@gmail.com>
2022-11-15 07:39:58 +0000

    Switched to custom toast notifications for errors

--- sjcadmin/static/app.js
+++ sjcadmin/static/app.js
@@ -1,3 +1,58 @@
+const toast = type => {
+    let t = document.createElement('div'),
+        tinner = document.createElement('div'),
+        tbody = document.createElement('div'),
+        btn = document.createElement('button');
+
+    t.classList.add('toast', 'align-items-center');
+    t.setAttribute('role', 'alert');
+    t.setAttribute('aria-live', 'assertive');
+    t.setAttribute('aria-atomic', 'true');
+    tinner.classList.add('d-flex');
+    tbody.classList.add('toast-body');
+    btn.setAttribute('type', 'button');
+    btn.classList.add('btn-close', 'btn-close-white', 'me-2', 'm-auto');
+    btn.dataset.bsDismiss = 'toast';
+    btn.setAttribute('aria-label', 'Close');
+
+    switch (type) {
+        case 'error':
+            t.classList.add('text-white', 'bg-danger');
+            break;
+    }
+
+    t.appendChild(tinner);
+    tinner.appendChild(tbody);
+    tinner.appendChild(btn);
+
+    return function (text) {
+        r = t.cloneNode(true);
+        r.querySelector('.toast-body').appendChild(document.createTextNode(text));
+        return r;
+    }
+}
+
+function notify(toast) {
+    document.getElementById('toastRack').appendChild(toast);
+    bootstrap.Toast.getOrCreateInstance(toast).show();
+}
+
+const postForm = form => url => fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(new FormData(form)).toString(),
+}).then(function (r) {
+    return r.json().then(function (d) {
+        if (d.hasOwnProperty('error')) {
+            return Promise.reject(new Error(d.error));
+        }
+        return d;
+    })
+});
+
+const error = toast('error');
+const handleError = text => notify(error(text));
+
 const frmNewMember = document.getElementById('frmNewMember'),
       btnNewMember = document.getElementById('btnNewMember');
 
@@ -19,24 +74,14 @@ if (frmNewMember && btnNewMember) {
 
     frmNewMember.addEventListener('click', function (e) {
         if (event.target && event.target.id == 'mdlNewMember_submit') {
-            fetch('/api/members/add', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: new URLSearchParams(new FormData(frmNewMember)).toString(),
-            }).then(function (r) {
-                r.json().then(function (d) {
-                    if (d.hasOwnProperty('error')) {
-                        alert(d.error);
-                        return
-                    }
-
-                    frmNewMember.reset();
-                    table.ajax.reload();
-                });
+            postForm(frmNewMember)('api/members/add').then(function (r) {
                 popNewMember.hide();
-            });
+                frmNewMember.reset();
+                table.ajax.reload();
+            }).catch(handleError);
 
             e.preventDefault();
         }
     });
 }
+
---
+++ sjcadmin/static/attendance.js
@@ -0,0 +1,177 @@
+const membershipBadge = (m, t, r) => {
+    const expired = sb => {
+        sb.classList.add('bg-danger');
+        sb.textContent = 'Expired';
+        return sb.outerHTML;
+    }
+
+    let b = document.createElement('span');
+    b.classList.add('badge');
+    switch(m) {
+        case 'licenced':
+            if (r.licence.exp) {
+                return expired(b);
+            }
+            b.classList.add('bg-success');
+            b.textContent = 'Licenced';
+            return b.outerHTML;
+        case 'trial':
+            if (r.rem_trial_sessions <= 0) {
+                return expired(b);
+            }
+            b.classList.add('bg-warning');
+            b.classList.add('text-dark');
+            b.textContent = 'Trial';
+            return b.outerHTML;
+    }
+};
+
+const attendanceBadge = r => date => {
+    let b = document.createElement('span'),
+        i = document.createElement('i'),
+        paid = r.paid.includes(date),
+        complementary = r.complementary.includes(date);
+    b.classList.add('badge');
+
+    if (paid || complementary) {
+        i.classList.add('fs-8');
+        i.classList.add('bi-check');
+        i.classList.add('me-1');
+        b.classList.add('bg-primary');
+        b.appendChild(i);
+        b.appendChild(document.createTextNode(paid ? 'Paid' : 'Free'));
+        return b.outerHTML;
+    }
+
+    b.classList.add('bg-secondary');
+    b.textContent = 'Attending';
+    return b.outerHTML;
+}
+
+const studentClassAttendance = date => (m, t, r) => {
+    return r.attendances.includes(date) ? attendanceBadge(r)(date): '';
+}
+
+const dataClasses  = JSON.parse(document.getElementById('dataClasses').textContent);
+
+const details = row => {
+    let list = document.createElement('ul'),
+        li   = (l, c) => {
+            let r = document.createElement('li'),
+                el = document.createElement('label');
+            el.appendChild(document.createTextNode(l + ': '));
+            r.appendChild(el);
+            r.appendChild(document.createTextNode(c));
+            return r;
+        },
+        manageLink = uuid => {
+            let i = document.createElement('i'),
+                a = document.createElement('a');
+
+            i.classList.add('bi');
+            i.classList.add('bi-box-arrow-up-right');
+            a.appendChild(i);
+            a.appendChild(document.createTextNode(' Manage'));
+            a.setAttribute('href', '/members/' + uuid);
+            return a;
+        };
+
+    list.classList.add('details');
+
+    switch(row['membership']) {
+        case 'licenced':
+            list.appendChild(li('Licence No.', row['licence']['no']));
+            list.appendChild(li('Licence Expires', row['licence']['exp_time']));
+            break;
+        case 'trial':
+            list.appendChild(li('Remaining Trial Sessions', row['rem_trial_sessions']));
+    }
+
+    list.appendChild(manageLink(row.uuid));
+
+    return list;
+}
+
+const table = $('.tblStudents').DataTable({
+    ajax: {
+        url: "/api/members",
+        dataSrc: "",
+    },
+    columns: [
+        {"data":"name", "className": "studentName"},
+        {"data":"membership", "render": membershipBadge, "className": "studentMembership"},
+    ].concat(dataClasses.map(function (c) {
+        return {
+            "className": "session",
+            "createdCell": (td, cd, rd, r, co) => {
+                td.setAttribute('data-sessdate', c.d);
+            },
+            "data": null,
+            "orderable": false,
+            "render": studentClassAttendance(c.d),
+        }
+    })),
+    paging: false,
+    scrollX: true,
+    scrollCollapse: true,
+});
+
+table.table().container().querySelector('.dataTables_filter')
+    .prepend(document.querySelector('#tblStudents_buttons'));
+
+$('.tblStudents').on('click', 'td.studentName,td.studentMembership', function (e) {
+    let row  = table.row(this),
+        data = row.data();
+
+    if (row.child.isShown()) {
+        row.child.hide();
+        return;
+    }
+
+    row.child(details(data)).show();
+});
+
+const mdlAttendanceInner = document.getElementById('mdlAttendance'),
+      mdlAttendance = new bootstrap.Modal(mdlAttendanceInner),
+      frmAttendance = document.getElementById('frmAttendance'),
+      btnMdlAttendanceCancel = document.getElementById('mdlAttendance_cancel');
+
+$('.tblStudents').on('click', 'td.session', function (e) {
+    let cell = table.cell(this).node(),
+        row = table.row(this).data(),
+        date = cell.getAttribute('data-sessdate');
+
+    mdlAttendanceInner.querySelector('#mdlAttendance_sessionDate').value = date;
+    mdlAttendanceInner.querySelector('#mdlAttendance_studentUuid').value = row.uuid;
+    mdlAttendanceInner.querySelector('#mdlAttendance_studentName').value = row.name;
+    mdlAttendanceInner.querySelector('#mdlAttendance_paid').checked = row.paid.includes(date);
+    mdlAttendanceInner.querySelector('#mdlAttendance_complementary').checked = row.complementary.includes(date);
+
+    mdlAttendance.show();
+});
+
+btnMdlAttendanceCancel.addEventListener('click', function () {
+    mdlAttendance.hide();
+});
+
+frmAttendance.addEventListener('submit', function (e) {
+    if (e.submitter) {
+        let submitter = e.submitter.id;
+        e.preventDefault();
+
+        switch (submitter) {
+            case 'mdlAttendance_submit':
+                postForm(frmAttendance)('/api/attendance/log').then(function (r) {
+                    table.ajax.reload();
+                    mdlAttendance.hide();
+                }).catch(handleError);
+                break;
+            case 'mdlAttendance_clear':
+                postForm(frmAttendance)('/api/attendance/clear').then(function (r) {
+                    table.ajax.reload();
+                    mdlAttendance.hide();
+                }).catch(handleError);
+                break;
+        }
+    }
+});
