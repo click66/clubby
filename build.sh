d9d1c17e6ff4ce11788868f392b64cc414047e16
Clark Sirl <click66@gmail.com>
2022-12-26 15:24:49 +0000

    Removed django static file collection (now unnecessary) from back end build pipeline

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ TARGET_DIR=${1:-.}
 docker-compose build
 
 # Back end
-docker-compose -f docker-compose.yml run app bash -c "python ./manage.py collectstatic --no-input && tar -zcvf app.tar.gz --files-from=buildfiles.txt"
+docker-compose -f docker-compose.yml run app bash -c "tar -zcvf app.tar.gz --files-from=buildfiles.txt"
 docker cp $(docker-compose ps -a -q app):/app/app.tar.gz "$TARGET_DIR/app.tar.gz"
 
 # Front end
