017c78c87a67bea74d10729037b2432840533369
Clark Sirl <click66@gmail.com>
2022-12-27 22:31:58 +0000

    Added course entities to admin screen

--- sjcadmin/admin.py
+++ sjcadmin/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models.student import Licence, Student, Payment
 from .models.attendance import Attendance
+from .models.course import Course
 
 admin.site.register(Licence)
 admin.site.register(Student)
 admin.site.register(Attendance)
 admin.site.register(Payment)
+admin.site.register(Course)
