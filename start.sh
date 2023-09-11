0022b97a898abdd6d9bec8c95958db82d6363932
Clark Sirl <click66@gmail.com>
2023-09-11 08:16:38 +0100

    Increased number of gunicorn workers

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 poetry run ./manage.py migrate
 
-poetry run gunicorn --reload sjcadmin.wsgi:application -b 0.0.0.0:8000
+poetry run gunicorn --workers 4 --reload sjcadmin.wsgi:application -b 0.0.0.0:8000
