91ea68740d66c631530f4f19fcf701b8b782e1f9
Clark Sirl <click66@gmail.com>
2023-09-24 12:56:38 +0100

    Added favicons. Removed currently unused Reporting link

--- frontend/src/pages/Portal.tsx
+++ frontend/src/pages/Portal.tsx
@@ -8,7 +8,7 @@ function Portal() {
             <li><Link to="/courses"><CollectionFill size={48} /><br />Courses</Link></li>
             <li><Link to="/members"><PeopleFill size={48} /><br />Members</Link></li>
             <li><Link to="/attendance"><CalendarFill size={48} /><br />Attendance</Link></li>
-            <li><Link to="/reporting"><BarChartFill size={48} /><br />Reporting</Link></li>
+            {/* <li><Link to="/reporting"><BarChartFill size={48} /><br />Reporting</Link></li> */}
         </ul>
     )
 }
