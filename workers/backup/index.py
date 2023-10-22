15fd8bc77b10e2a8ca09cec2e13d65da0c7be813
Clark Sirl <click66@gmail.com>
2023-10-22 06:51:58 +0100

    Add backup worker

---
+++ workers/backup/index.py
@@ -0,0 +1,46 @@
+import os
+import boto3
+import logging
+import psycopg2
+import subprocess
+
+DB_HOST = "10.0.0.186"
+S3_BUCKET = os.environ.get("S3_BUCKET")
+S3_KEY = "backup.sql"
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def handler(event, context):
+    logger.info('Invoking handler')
+    ssm = boto3.client('ssm')
+    db_password = ssm.get_parameter(Name="sjcadmin_PGPASS", WithDecryption=True)[
+        'Parameter']['Value']
+
+    backup_database("southamptonjiujitsu", db_password)
+    backup_database("sjcattendance", db_password)
+
+
+def backup_database(database_name, db_password):
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=database_name,
+            user=database_name,
+            password=db_password,
+        )
+
+        cmd = f"pg_dump -h {DB_HOST} -U {database_name} {database_name} -f /tmp/{database_name}.sql"
+        subprocess.run(cmd, shell=True, check=True)
+
+        s3 = boto3.client('s3')
+        s3.upload_file(f"/tmp/{database_name}.sql",
+                       S3_BUCKET, f"{S3_KEY}/{database_name}.sql")
+
+    except Exception as e:
+        print(f"Error backing up {database_name} database: {str(e)}")
+
+    finally:
+        if conn:
+            conn.close()
