701e6171717cc0de0519e9de88f53c0a2688ca39
Clark Sirl <click66@gmail.com>
2023-11-05 09:50:36 +0000

    Updated mock of tokens used in tests

--- frontend/tests/domain/mock-http.ts
+++ frontend/tests/domain/mock-http.ts
@@ -2,9 +2,17 @@ import MockAdapter from 'axios-mock-adapter'
 import { HttpInstance, withInterceptors } from '../../src/utils/http'
 
 const tokens = new class {
-    get(_: string) {
+    getAuthorisationToken() {
         return 'foo'
     }
+    getRefreshToken() {
+        return 'bar'
+    }
+    exist() {
+        return true
+    }
+    setToken(_: any) { }
+    clear() { }
 }
 
 export default (http: HttpInstance) => new MockAdapter(withInterceptors(http, tokens))
