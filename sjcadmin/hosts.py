8f88c033e71c83ee15bf3c4b8a68c083df821cfc
Clark Sirl <click66@gmail.com>
2023-11-05 12:56:11 +0000

    Removed unused members host

--- sjcadmin/hosts.py
+++ sjcadmin/hosts.py
@@ -3,6 +3,4 @@ from django.conf import settings
 
 host_patterns = patterns('',
                          host(r'admin', 'sjcadmin.sjcadmin.urls', name='admin'),
-                         host(r'members', 'sjcadmin.sjcmembers.urls',
-                              name='members'),
                          )
